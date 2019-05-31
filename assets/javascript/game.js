038435505c6f55e146034b2a8cd7601d1060617f
Jessica Sewell <jessicadawnsewell@gmail.com>
2019-05-31 10:07:07 -0600

    code resturcture

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,48 +3,53 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var guesses = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var guessesSoFar = [];
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// Create variables that hold references to the places in the HTML where we want to display things.
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
+// var directionsText = document.getElementById("directions-text");
 var computerChoiceText = document.getElementById("computerchoice-text")
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guesses-left");
-var guessesText = document.getElementById("guesses-count");
+var guessesSoFarText = document.getElementById("guessesSoFar");
+// var computerLetter = computerGuess(computerChoices);
 
-function computerGuess(computerChoices) {
-    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+function resetGame() {
+    // guessesLeftText.textContent = 10;
+    guessesSoFar = [];
+    // guessesSoFarText.textContent = "";
+    // guessCount = 0;
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
-var computerLetter = computerGuess(computerChoices);
+window.onload = function(){
+    resetGame();
+}
 
-console.log(computerLetter);
+// console.log(computerLetter);
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
     console.log(event.key);
     var userGuess = event.key;
-    
-    if (userGuess === computerLetter) {
-        wins++;
-        console.log("You win!")
-    } else {
-        (losses++) && (guessesLeft--);
-        console.log("You lose.")
-    }
-    
-    document.getElementById("userchoice-text").textContent = userGuess;
-    // // computerChoiceText.textContent = "";
-    document.getElementById("guesses-left").textContent = "Guesses Left: " + guessesLeft;
-    document.getElementById("wins-text").textContent = "Wins: " + wins;
-    document.getElementById("losses-text").textContent = "Losses: " + losses;
-    document.getElementById("guesses-count").textContent = "Your Guesses so far: ";
 
-    // function reset(gameOver) {
-    //     if (guessesLeft = 0) {
-    //     console.log("Game Over")
-    //     }
-    // }
+    if(computerChoices.indexOf(userGuess) > -1) {
+        if (guessesSoFar.indexOf(userGuess) > -1);
+            alert("Choose a different letter...");
+    } else {
+        guessesSoFar.push(userGuess); //push into the array
+        userChoiceText.textContent = guessesSoFar.join(); //join addes the commas
+        guessesLeftText.textContent = guessesLeft - guessesSoFar.length; 
+
+        if (userGuess === computerGuess && guessesSoFar.length < 10) {
+            wins++;
+            winsText.textContent = wins;
+            alert ("Good guess!" + computerGuess);
+            resetGame();
+        } else if (userGuess !== computerGuess && guessesLeft === 0) {
+            losses++;
+            lossesText.textContent = losses;
+            alert ("Try again..." + computerGuess);
+            resetGame();
+        }
+    }
 }
